2907c4430bb344ed1a6a4f81b0809ec7e7d4a566
Eren OZDEK <eozdek@e2r2p13.42.fr>
2017-07-18 17:00:02 +0200

    rien de fou

--- test/main.py
+++ test/main.py
@@ -46,33 +46,44 @@ def letterDicValue(equal, letterFile):
     dic = {}
     for i in letterFile:
         if equal[0].find(i) != -1:
-            dic[i] = {"letter": i, "val": True, "constant":True}
+            dic[i] = {"letter": i, "val": True, "constant": True}
         else:
-            dic[i] = {"letter": i, "val": False, "constant":False}
+            dic[i] = {"letter": i, "val": False, "constant": None}
     return dic
 
+# def determineBool(left, right, dicEqu, dic):
+#     # print "a"
+#     i = 0
+#     for key in dic:
+#         if dic[key]["val"] == True:
+#             right[0]
+#         i += 1
+
 def printAll(dicEqu, dic, left, right, equal, query, letterFile, equ, letterLine):
-    print("dicEqu", dicEqu)
-    print("left", left)
-    print("right", right)
-    print("ewu" ,equ)
-    print("letterLine" , letterLine)
-    print("equal[0]", equal[0])
-    print("query", query)
-    print("letterFile0", letterFile)
-    print("dic", dic)
+    print "dicEqu", dicEqu
+    print "left", left
+    print "right", right
+    print "ewu" ,equ
+    print "letterLine" , letterLine
+    print "equal[0]", equal
+    print "query", query
+    print "letterFile", letterFile
+    print "dic", dic
 
 def main(argv):
     file = open(argv[0], 'r')
     regex = re.compile(r"#.*", re.IGNORECASE)
     file2 = regex.sub("", file.read())
     file2 = file2.replace(" ", "")
-    # print(file2)
+    print(file2)
     left = re.findall(".*[A-Z]\s*(?=\=>)|.*[A-Z]\s*(?=<\=>)", file2)
     right = re.findall("(?<=\=>).*[A-Z]\s*(?=\n)|(?<=<\=>).*[A-Z]\s*(?=\n)", file2)
     equ = re.findall("=>|<=>", file2)
     equal = re.findall("(?<=\n=).*", file2)
     query = re.findall("(?<=\n\?).*", file2)
+
+    #pour split le string en lettre
+    equal = list(equal[0])
 
     # tableau tous les lettre pour chaque ligne
     letterLine = letterForEachLine(file2)
@@ -86,6 +97,7 @@ def main(argv):
 
     # dictionnaire des lettres avec leurs valeurs
     dic = letterDicValue(equal, letterFile)
+    # putLettersToTrue(dic, equal)
     printAll(dicEqu, dic, left, right, equal, query, letterFile, equ, letterLine)
 
 if __name__ == "__main__":
